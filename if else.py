87ef8d6da34bd9634cf03e5538cc9a6ab3655457
aaryan1845 <166988624+aaryan1845@users.noreply.github.com>
2024-06-19 18:30:52 +0530

    if else code

---
+++ if else.py
@@ -0,0 +1,11 @@
+age = 2
+if age >= 18:
+    print("you are adult")
+    print("you can vote")
+elif age < 18 and  age > 3:
+    print("you are school student")
+    print("you are not adult")
+else:
+    print("you are child")
+    print("you are pre school student")
+print("Thank you")
