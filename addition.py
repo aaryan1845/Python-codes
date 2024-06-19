b1a5c8baeec38809953f537604fe88ae38359f8c
aaryan1845 <166988624+aaryan1845@users.noreply.github.com>
2024-06-19 18:30:22 +0530

    Addition code of python

---
+++ addition.py
@@ -0,0 +1,15 @@
+def add_num(a,b):
+   sum = int(a)+ int(b)
+   return sum
+#Addition operations
+num1 = input('Enter the first Number : ')
+num2 = input("Enter the second number : ")
+
+sum = int(num1)+int(num2)
+print("Sum of two number is: ",sum )
+#sum = int(num1 + num2) it also cant work in addition.
+#print("The the sum of two no is :",add_num(num1,num2))
+
+#range
+number = range(sum)
+print(number)
